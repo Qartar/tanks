e176baad1bfbc6e0dbc1a36fbe04c66560f2fdc4
Carter Bray <qartar@live.com>
2019-01-10 19:43:05 -0800

    Miscellaneous changes relative to 0a885ff5

--- game/g_particles.cpp
+++ game/g_particles.cpp
@@ -133,12 +133,12 @@ void world::add_effect(time_value time, effect_type type, vec2 position, vec2 di
                     return;
 
                 r = _random.uniform_real(2.f * math::pi<float>);
-                d = _random.uniform_real(8.f) * scale;
+                d = _random.uniform_real(8.f * scale);
 
                 p->position = position + vec2(cos(r),sin(r))*d;
 
-                r = _random.uniform_real()*math::pi<float>*2.0f;
-                d = sqrt(_random.uniform_real()) * 128.0f * strength;
+                r = _random.uniform_real(2.f * math::pi<float>);
+                d = sqrt(_random.uniform_real()) * 128.f * strength;
 
                 p->velocity = vec2(cos(r),sin(r))*d;
                 p->velocity += direction * d * 0.5f;
@@ -162,14 +162,14 @@ void world::add_effect(time_value time, effect_type type, vec2 position, vec2 di
 
                 p->position = position + vec2(cos(r)*d,sin(r)*d);
 
-                r = _random.uniform_real(math::pi<float>*2.0f);
-                d = _random.uniform_real(128.f) * scale;
+                r = _random.uniform_real(2.f * math::pi<float>);
+                d = _random.uniform_real(128.f * scale);
 
                 p->velocity = vec2(cos(r)*d,sin(r)*d);
                 p->velocity += direction * d * 0.5f;
 
                 p->color = color4(1,_random.uniform_real(.5f, 1.f),0,1);
-                p->color_velocity = color4(0,0,0,-1.5f-_random.uniform_real());
+                p->color_velocity = color4(0,0,0,_random.uniform_real(-2.5f, -1.5f));
                 p->size = 0.5f;
                 p->size_velocity = 0.0f;
                 p->drag = _random.uniform_real(.5f, 1.f);
@@ -206,13 +206,13 @@ void world::add_effect(time_value time, effect_type type, vec2 position, vec2 di
                 p->position = position + vec2(cos(r),sin(r))*d;
 
                 r = _random.uniform_real(2.f * math::pi<float>);
-                d = sqrt(_random.uniform_real()) * 32.0f;
+                d = sqrt(_random.uniform_real()) * 32.f;
 
                 p->velocity = vec2(cos(r),sin(r))*d;
                 p->velocity += direction * d * 0.5f;
 
                 p->color = color4(1.0f,_random.uniform_real(.25f, .75f),0.0f,0.1f);
-                p->color_velocity = color4(0,0,0,-p->color.a/(.25f+square(_random.uniform_real())));
+                p->color_velocity = color4(0,0,0,-p->color.a/(0.25f+square(_random.uniform_real())));
                 p->size = _random.uniform_real(2.f, 6.f);
                 p->size_velocity = 0.5f;
                 p->flags = render::particle::invert;
@@ -454,7 +454,7 @@ void world::add_trail_effect(effect_type type, vec2 position, vec2 old_position,
 
                 r = _random.uniform_real(2.f * math::pi<float>);
                 d = _random.uniform_real();
-                p->position = position + vec2(std::cos(r)*d,std::sin(r)*d) + lerp * t;
+                p->position = position + vec2(std::cos(r)*d,std::sin(r)*d) + lerp * static_cast<float>(ii) / static_cast<float>(count);
                 p->velocity = direction * _random.uniform_real(.25f, .75f) + vec2(_random.uniform_real(-24.f, 24.f),_random.uniform_real(-24.f, 24.f));
 
                 p->size = _random.uniform_real(1.f, 2.f);
@@ -476,7 +476,7 @@ void world::add_trail_effect(effect_type type, vec2 position, vec2 old_position,
                 r = _random.uniform_real(2.f * math::pi<float>);
                 d = _random.uniform_real();
 
-                p->position = position + vec2(std::cos(r),std::sin(r))*d + lerp * t;
+                p->position = position + vec2(std::cos(r),std::sin(r))*d + lerp * static_cast<float>(ii) / static_cast<float>(count);
 
                 r = _random.uniform_real(2.f * math::pi<float>);
                 d = _random.uniform_real(64.f);
